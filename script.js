c85b4d5f3e0d802f367ede3c586cf44af1669160
Aleksei454545 <vaseikoalex@gmail.com>
2025-10-12 14:53:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,86 +2,88 @@ const $btn = document.getElementById('btn-kick');
 const $btnUlt = document.getElementById('btn-ult');
 
 const character = {
-    name: 'Pikachu',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-character'),
-    elProgressbarHP: document.getElementById('progressbar-character'),
+  name: 'Pikachu',
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById('health-character'),
+  elProgressbarHP: document.getElementById('progressbar-character'),
 };
 
 const enemy = {
-    name: 'Charmander',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-enemy'),
-    elProgressbarHP: document.getElementById('progressbar-enemy'),
+  name: 'Charmander',
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById('health-enemy'),
+  elProgressbarHP: document.getElementById('progressbar-enemy'),
 };
 
 const enemy2 = {
-    name: 'Bulbasaur',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-enemy2'),
-    elProgressbarHP: document.getElementById('progressbar-enemy2'),
+  name: 'Bulbasaur',
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById('health-enemy2'),
+  elProgressbarHP: document.getElementById('progressbar-enemy2'),
 };
 
+function renderHP() {
+  this.renderHPLife();
+  this.renderProgressbarHP();
+}
+
+function renderHPLife() {
+  this.elHP.innerText = `${this.damageHP} / ${this.defaultHP}`;
+}
+
+function renderProgressbarHP() {
+  const percent = (this.damageHP / this.defaultHP) * 100;
+  this.elProgressbarHP.style.width = percent + '%';
+
+  if (percent > 60) {
+    this.elProgressbarHP.className = 'health';
+  } else if (percent > 20) {
+    this.elProgressbarHP.className = 'health low';
+  } else {
+    this.elProgressbarHP.className = 'health critical';
+  }
+}
+
+function changeHP(count) {
+  if (this.damageHP <= count) {
+    this.damageHP = 0;
+    this.renderHP();
+    alert('Бідний ' + this.name + ' програв бій!');
+    $btn.disabled = true;
+    $btnUlt.disabled = true;
+  } else {
+    this.damageHP -= count;
+    this.renderHP();
+  }
+}
+
+const sharedMethods = { renderHP, renderHPLife, renderProgressbarHP, changeHP };
+Object.assign(character, sharedMethods);
+Object.assign(enemy, sharedMethods);
+Object.assign(enemy2, sharedMethods);
 
 const players = [character, enemy, enemy2];
 
-
 $btn.addEventListener('click', function () {
-    console.log('Thunder Jolt!');
-    players.forEach(p => changeHP(random(20), p));
+  console.log('Thunder Jolt!');
+  players.forEach(p => p.changeHP(random(20)));
 });
 
 $btnUlt.addEventListener('click', function () {
-    console.log('Ult Kick!');
-    players.forEach(p => changeHP(random(35), p));
+  console.log('Ult Kick!');
+  players.forEach(p => p.changeHP(random(35)));
 });
 
 function init() {
-    console.log('Start Game!');
-    players.forEach(renderHP);
-}
-
-function renderHP(person) {
-    renderHPLife(person);
-    renderProgressbarHP(person);
-}
-
-function renderHPLife(person) {
-    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
-}
-
-function renderProgressbarHP(person) {
-    const percent = (person.damageHP / person.defaultHP) * 100;
-    person.elProgressbarHP.style.width = percent + '%';
-
-
-    if (percent > 60) {
-        person.elProgressbarHP.className = 'health'; 
-    } else if (percent > 20) {
-        person.elProgressbarHP.className = 'health low'; 
-    } else {
-        person.elProgressbarHP.className = 'health critical'; 
-    }
-}
-
-function changeHP(count, person) {
-    if (person.damageHP <= count) {
-        person.damageHP = 0;
-        renderHP(person);
-        alert('Бедный ' + person.name + ' проиграл бой!');
-        $btn.disabled = true;
-        $btnUlt.disabled = true;
-    } else {
-        person.damageHP -= count;
-        renderHP(person);
-    }
+  console.log('Start Game!');
+  players.forEach(p => p.renderHP());
 }
 
 function random(num) {
-    return Math.ceil(Math.random() * num);
+  return Math.ceil(Math.random() * num);
 }
 
 init();
